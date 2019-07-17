6b984f55287c96d73e25841d7b50099e4099009b
Chris Gardner <chris.gardner@section8games.co.uk>
2019-07-17 20:15:59 +0100

    Add support for remote computers

--- example.ps1
+++ example.ps1
@@ -1,10 +1,27 @@
 function Get-Data {
-    [PSCustomObject]@{
-        Name = $Env:COMPUTERNAME
-        OS = (Get-ComputerInfo).OSName
-        IPAddress = (Get-NetIPAddress -AddressFamily IPv4 -InterfaceAlias 'Ethernet').IPAddress
-        Domain = (Get-ComputerInfo).CsDomain
-        Model = (Get-ComputerInfo).Model
-        Manufacturer = (Get-ComputerInfo).Manufacturer
+    param (
+        $ComputerName = $Env:COMPUTERNAME
+    )
+    if ($ComputerName -eq $Env:COMPUTERNAME) {
+        [PSCustomObject]@{
+            Name = $Env:COMPUTERNAME
+            OS = (Get-ComputerInfo).OSName
+            IPAddress = (Get-NetIPAddress -AddressFamily IPv4 -InterfaceAlias 'Ethernet').IPAddress
+            Domain = (Get-ComputerInfo).CsDomain
+            Model = (Get-ComputerInfo).Model
+            Manufacturer = (Get-ComputerInfo).Manufacturer
+        }
+    }
+    else {
+        Invoke-Command -ComputerName $ComputerName -ScriptBlock {
+            [PSCustomObject]@{
+                Name = $Env:COMPUTERNAME
+                OS = (Get-ComputerInfo).OSName
+                IPAddress = (Get-NetIPAddress -AddressFamily IPv4 -InterfaceAlias 'Ethernet').IPAddress
+                Domain = (Get-ComputerInfo).CsDomain
+                Model = (Get-ComputerInfo).Model
+                Manufacturer = (Get-ComputerInfo).Manufacturer
+            }
+        }
     }
 }
