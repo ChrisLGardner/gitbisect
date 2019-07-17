7f5bd9fc096ebaaf73ad61e6f48681a11d8023fe
Chris Gardner <chris.gardner@section8games.co.uk>
2019-07-17 20:11:50 +0100

    Add initial function

---
+++ example.ps1
@@ -0,0 +1,7 @@
+function Get-Data {
+    [PSCustomObject]@{
+        Name = $Env:COMPUTERNAME
+        OS = (Get-ComputerInfo).OSName
+        IPAddress = (Get-NetIPAddress -AddressFamily IPv4 -InterfaceAlias 'Ethernet').IPAddress
+    }
+}
